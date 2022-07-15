0c11a06a204983cae1599b19b67d803635b89ed7
silentsoft <hyesung.lee@silentsoft.org>
2022-07-15 15:55:13 +0900

    Fix security hotspots

--- src/main/java/org/silentsoft/solarguard/util/LicenseUtil.java
+++ src/main/java/org/silentsoft/solarguard/util/LicenseUtil.java
@@ -2,19 +2,17 @@ package org.silentsoft.solarguard.util;
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
 
-import java.util.Random;
-
 public class LicenseUtil {
 
     public static final char[] CHARACTERS = "ABCDEFHJKMNPRTWXY34678".toCharArray();
 
     public static String generateLicenseKey() {
-        String licenseKey = NanoIdUtils.randomNanoId(new Random(), CHARACTERS, 25);
+        String licenseKey = NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, CHARACTERS, 25);
         return String.join("-", licenseKey.substring(0, 5), licenseKey.substring(5, 10), licenseKey.substring(10, 15), licenseKey.substring(15, 20), licenseKey.substring(20, 25));
     }
 
     public static String generateDeviceCode() {
-        return NanoIdUtils.randomNanoId(new Random(), CHARACTERS, 10);
+        return NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, CHARACTERS, 10);
     }
 
 }
